f21a2d14b27552a9fe64cd313d2410478cd94c52
moses <mking78911@gmail.com>
2024-09-17 23:48:10 +0100

    alternate casing

---
+++ alternate_casing.py
@@ -0,0 +1,11 @@
+def to_alternating_case(string):
+    result = ""
+    for letter in string:
+        #Check to see if the letter is lower
+        if letter.islower():
+            #Converts lowercase to uppercase
+            result += letter.upper()
+        else:
+            #Converts uppercase to lowercase
+            result += letter.lower()
+    return result
