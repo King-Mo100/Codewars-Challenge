ccc7fe4e9a3bb05c199a92b1b17070a5e75f499f
moses <mking78911@gmail.com>
2024-09-21 12:54:22 +0100

    codewars calculate BMI challenge

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -7,3 +7,24 @@ bmi = weight / (height ** 2)
 
 # Print the result
 print(f"Your BMI is: {round(bmi, 2)}")
+
+#Codewars challenge
+
+def bmi(weight, height):
+    #your code here
+    bmi= weight/(height**2)
+    
+    if bmi <= 18.5:
+        return "Underweight"
+
+    elif bmi <= 25.0:
+        return "Normal"
+
+    elif bmi <= 30.0:
+        return "Overweight"
+
+    elif bmi > 30:
+        return "Obese"
+
+# fucntion call example
+print(bmi(70, 1.75))
